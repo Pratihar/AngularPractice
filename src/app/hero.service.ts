c4e9d37d715cb57e96832cb9bf80c8f8a2a096f9
Pratihar <dewanshu.pratihar@gmail.com>
2018-10-03 21:48:09 +0530

    Search Implemented. End of tutorial :)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,7 +4,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http' 
-import { catchError, map, tap} from 'rxjs/operators'
+import { catchError, map, tap, filter} from 'rxjs/operators'
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -26,6 +26,36 @@ export class HeroService {
 
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  searchHeroes(term : string): Observable<Hero[]> {
+    if(!term.trim())
+    {
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_=>this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+  
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  addHero (hero: Hero): Observable<Hero>{
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero:Hero) => this.log(`added hero, id=${hero.id}`)),
+      catchError(this.handleError<any>('addHero'))
+    )
+
   }
 
   updateHero (hero: Hero): Observable<any> {
